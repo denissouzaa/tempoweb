c96532d0dd602b888e6ac5052c34f785081a62f8
Denis de Souza Andrade <denis.ilhabela@gmail.com>
2021-11-30 19:41:04 -0300

    u

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,19 +4,15 @@ import NavComponent from '../../components/nav'
 
 import api from '../../services/api'
 
-const apiKey = '18b2ee8e1f19d5c929651890aec86896'
-
 export default function Home() {
 
     const [dadosApi, setDadosApi] = useState([])
 
     useEffect(() => {
-        api.get("http://api.openweathermap.org/data/2.5/weather?q=Ilhabela&appid=18b2ee8e1f19d5c929651890aec86896&lang=pt_br")
-        .then(({data}) => setDadosApi(data))
+        api.get("/weather?q=Ilhabela&appid=18b2ee8e1f19d5c929651890aec86896&lang=pt_br")
+        .then(({data}) => {setDadosApi(data)})
         .catch((error) => {console.log("Algo deu errado " + error)})
     }, [])
-
-    console.log(dadosApi)
 
     return (
         <>
@@ -27,11 +23,11 @@ export default function Home() {
                 <Container>
                     <div className="mt-4">
                         <div className="borderCaixaCupom bgWhite p-2 d-flex flex-column border border-1 align-items-center">
-                            {dadosApi.map((item) => (<p></p>))
-                        }
-                            <div><img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" /></div>
-                            <div className="mt-2 fs-1">25.86°C</div>
-                            <div className="mt-2 text-center">Chuva Leve<br/>
+                        
+
+                            <div><img alt="Clima Status" src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" /></div>
+                            <div className="mt-2 fs-1">123</div>
+                            <div className="mt-2 text-center">123<br/>
                                 Sensação termica: 26.43°C</div>
 
                         </div>
